55176375f97dd12b35e995d96dfee7995fc4c8f6
Meg Shapiro <mshapiro@mines.edu>
2019-11-30 18:25:39 -0700

    Added GUI to embedded version

--- move_project.py
+++ move_project.py
@@ -1,10 +1,21 @@
 import numpy as np
 import pandas as pd
+import tkinter as tk
+import tkinter.ttk as ttk
+
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 import json
 import re
 from io import StringIO
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, HashingVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.metrics import classification_report, f1_score, accuracy_score
+from sklearn.svm import LinearSVC
+
 import operator
 io = StringIO()
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
@@ -106,7 +117,7 @@ pop_genres = ["Drama", "Comedy", "Thriller", "Action", "Adventure", "Romance", "
 
 listings = ['genres', 'production_companies', 'production_countries', 'spoken_languages', 'cast', 'crew']
 max_entries_movies = 3
-min_movie = [10, 10, 10, 10, 10, 10] #edit crew members later
+min_movie = [10, 100, 10, 10, 500, 500] #edit crew members later
 lists = ['gen_list', 'producers_list', 'countries_list', 'lang_list', 'cast_list', 'crew_list']
 success_list = pd.Series([])
 success_dicts = pd.Series([])
@@ -118,13 +129,7 @@ for i in range(len(listings)):
     list3 = pd.Series([])
     print(listings[i])
     for index, row in mov.iterrows():
-    #print(row['title'])
-        #print(i)
-        #print(listings[i])
         listString = row[listings[i]]
-        #if listString == "nan":
-        #    continue
-        #print(listString)
         l = re.findall('\'name\': \'[0-9a-zA-Z ]*\'', listString)
         for j in range(len(l)):
             st = l[j]
@@ -137,7 +142,6 @@ for i in range(len(listings)):
                     successDict[st] += 1
                 else:
                     successDict[st] = 1
-        #print(l)
         list1[index] = l
     mov[lists[i]] = list1
     # Create a list of tuples sorted by index 1 i.e. value field
@@ -145,18 +149,8 @@ for i in range(len(listings)):
 
     # Iterate over the sorted sequence
     for j in range(0, min_movie[i]):
-        #print(str(listofTuples[j][1]) + "::" + listofTuples[j][0])
         list2.append(listofTuples[j][0])
-
-
     top_lists[lists[i]] = list2
-    #success_list[i] = listofTuples
-    #success_dicts[i] = successDict
-
-
-#print(top_lists)
-#print(mov[lists].describe(include="all"))
-print(mov[lists].describe(include="all"))
 
 for i in range(len(lists)):
     list_name = lists[i]
@@ -180,90 +174,172 @@ for i in range(len(lists)):
         list_embed[index] = list_dict[s]
         mov.at[index, list_name] = list3
     mov[list_name+'_val'] = list_embed
-print(mov[lists].describe(include="all"))
 lists_val = ['gen_list_val', 'producers_list_val', 'countries_list_val', 'lang_list_val', 'cast_list_val', 'crew_list_val']
-print(mov[lists_val])
-''' 
-
-    all_genres = {}
-    #print(row['genres'])
-    stringG = row['genres'].replace('\'', '"')  # Data uses single quotes, but json interp needs double quotes
-    #print("stringG: ")
-    #print(stringG)
-    mov_gen = []
-    l = json.loads(stringG)  # create list of dictionaries
-    #print("l: ")
-    #print(l)
-    for index2 in range(len(l)):
-        # print(l[index2])
-        if l[index2]['name'] in pop_genres:
-            mov_gen.append(l[index2]['name'])
-        if l[index2]['name'] in all_genres:
-            all_genres[l[index2]['name']] = all_genres[l[index2]['name']] + 1
-        else:
-            all_genres[l[index2]['name']] = 1
-    if len(mov_gen) == 0:
-        mov_gen = ["Other"]
-    gen_list[index] = mov_gen
-
-    #print("stringCast: ")
-    stringCast = row['cast']
+#print(mov[lists_val].describe(include="all"))
 
 
-    mov_cast = re.findall('\'name\': \'[0-9a-zA-Z ]*\',', stringCast)
-    #print(cast)
-    for i in range(len(mov_cast)):
-        #print(st)
-        st = mov_cast[i]
-        st = re.sub('\'name\': \'', '', st)
-        st = re.sub('\',', '', st)
-        mov_cast[i] = st
-        #print(st)
-    #print(mov_cast)
-    cast_list[index] = mov_cast
+target = ['success']
+X_train, X_test, y_train, y_test = train_test_split(mov[lists_val], mov['success'], test_size=0.2, random_state=0)
 
-    stringCrew = row['crew']
+tfidfer = TfidfTransformer()
+X_train_tfidf = tfidfer.fit_transform(X_train)
+X_test_tfidf = tfidfer.transform(X_test)
 
+svm = LinearSVC().fit(X_train_tfidf, y_train)
+y_pred = svm.predict(X_test_tfidf)
+print(classification_report(y_test, y_pred))
 
-    #if (row['title'] == "7.1"): continue
-
-    #print(stringCrew)
-    mov_crew = re.findall('\'name\': \'[0-9a-zA-Z ]*\',', stringCrew)
-    #print(mov_crew)
-    for i in range(len(mov_crew)):
-        #print(st)
-        st = mov_crew[i]
-        st = re.sub('\'name\': \'', '', st)
-        st = re.sub('\',', '', st)
-        mov_crew[i] = st
-        #print(st)
-    #print(mov_crew)
-    crew_list[index] = mov_crew
-
-    stringProducers = row['production_companies']
-    mov_producers = re.findall('\'name\': \'[0-9a-zA-Z ]*\',', stringProducers)
-    #print(mov_producers)
-    for i in range(len(mov_producers)):
-        # print(st)
-        st = mov_producers[i]
-        st = re.sub('\'name\': \'', '', st)
-        st = re.sub('\',', '', st)
-        mov_producers[i] = st
-        # print(st)
-    #print(mov_producers)
-    producers_list[index] = mov_producers
-
-mov['gen_list'] = gen_list
-mov['cast_list'] = cast_list
-mov['crew_list'] = crew_list
-mov['producers_list'] = producers_list
-#print(all_genres)
 '''
+def reg():
+    str_all = ''
+    test = []
+    str_all = e_genres.get() + " " + e_procom.get() + " " + e_procount.get() + " " + e_cast.get() + " " + e_crew.get() +  " "
+    str_all = str_all + " " + e_spl.get()
+    test.append(str_all)
+    print(X_train.shape)
+    X_tr_tfidf = tfidfer.transform(X_train)
+    pred = svm.predict(X_tr_tfidf)
+    print(pred)
+    if pred:
+        l_msg['text'] = 'your movie will be successful'
+    else:
+        l_msg['text'] = 'your movie will not be successful'
+'''
+root = tk.Tk()
+root.title("Drop-down boxes for option selections.")
+
+root.geometry('500x500')
+root.title("movie prediction")
 
 
+l_msg = tk.Label(root, text='INPUT NEW MOVIE INFORMATION')
+l_msg.grid(row=0, columnspan=3)
+# cast
+l_cast1 = tk.Label(root, text='Select Actor 1:')
+l_cast1.grid(row=2, sticky=tk.W)
+e_cast1 = tk.StringVar(root)
+e_cast1.set("")
+a1 = ttk.Combobox(root, value=sorted(top_lists['cast_list']), textvariable=e_cast1)
+a1.grid(row=2, column=2, sticky=tk.E, padx=3)
+# cast 2
+l_cast2 = tk.Label(root, text='Select Actor 2:')
+l_cast2.grid(row=3, sticky=tk.W)
+e_cast2 = tk.StringVar(root)
+e_cast2.set("")
+a2 = ttk.Combobox(root, value=sorted(top_lists['cast_list']), textvariable=e_cast2)
+a2.grid(row=3, column=2, sticky=tk.E, padx=3)
+# cast 3
+l_cast3 = tk.Label(root, text='Select Actor 3:')
+l_cast3.grid(row=4, sticky=tk.W)
+e_cast3 = tk.StringVar(root)
+e_cast3.set("")
+a3 = ttk.Combobox(root, value=sorted(top_lists['cast_list']), textvariable=e_cast3)
+a3.grid(row=4, column=2, sticky=tk.E, padx=3)
 
-mov.drop(listings, axis=1, inplace=True)
-#print list of words to file
-#mov[lists+numeric_features].to_csv('processed.csv')
-#success_list.to_csv('success_lists.csv')
+# crew
+l_crew1 = tk.Label(root, text='Select Crew Member 1:')
+l_crew1.grid(row=5, sticky=tk.W)
+e_crew1 = tk.StringVar(root)
+e_crew1.set("")
+c1 = ttk.Combobox(root, value=sorted(top_lists['crew_list']), textvariable=e_crew1)
+c1.grid(row=5, column=2, sticky=tk.E, padx=3)
+# cast 2
+l_crew2 = tk.Label(root, text='Select Crew Member 2:')
+l_crew2.grid(row=6, sticky=tk.W)
+e_crew2 = tk.StringVar(root)
+e_crew2.set("")
+c2 = ttk.Combobox(root, value=sorted(top_lists['crew_list']), textvariable=e_crew2)
+c2.grid(row=6, column=2, sticky=tk.E, padx=3)
+# cast 3
+l_crew3 = tk.Label(root, text='Select Crew Member 3:')
+l_crew3.grid(row=7, sticky=tk.W)
+e_crew3 = tk.StringVar(root)
+e_crew3.set("")
+c3 = ttk.Combobox(root, value=sorted(top_lists['crew_list']), textvariable=e_crew3)
+c3.grid(row=7, column=2, sticky=tk.E, padx=3)
 
+# genres
+l_genre1 = tk.Label(root, text='Select Genre 1:')
+l_genre1.grid(row=8, sticky=tk.W)
+e_genre1 = tk.StringVar(root)
+e_genre1.set("")
+g1 = ttk.Combobox(root, value=sorted(top_lists['gen_list']), textvariable=e_genre1)
+g1.grid(row=8, column=2, sticky=tk.E, padx=3)
+# genre 2
+l_genre2 = tk.Label(root, text='Select Genre 2:')
+l_genre2.grid(row=9, sticky=tk.W)
+e_genre2 = tk.StringVar(root)
+e_genre2.set("")
+g2 = ttk.Combobox(root, value=sorted(top_lists['gen_list']), textvariable=e_genre2)
+g2.grid(row=9, column=2, sticky=tk.E, padx=3)
+# genre 3
+l_genre3 = tk.Label(root, text='Select Genre 3:')
+l_genre3.grid(row=10, sticky=tk.W)
+e_genre3 = tk.StringVar(root)
+e_genre3.set("")
+g3 = ttk.Combobox(root, value=sorted(top_lists['gen_list']), textvariable=e_genre3)
+g3.grid(row=10, column=2, sticky=tk.E, padx=3)
+
+# production_company 1
+l_procom1 = tk.Label(root, text='Select Producer 1:')
+l_procom1.grid(row=11, sticky=tk.W)
+e_procom1 = tk.StringVar(root)
+e_procom1.set("")
+p1 = ttk.Combobox(root, value=sorted(top_lists['producers_list']), textvariable=e_procom1)
+p1.grid(row=11, column=2, sticky=tk.E, padx=3)
+# production_company 2
+l_procom2 = tk.Label(root, text='Select Producer 2:')
+l_procom2.grid(row=12, sticky=tk.W)
+e_procom2 = tk.StringVar(root)
+e_procom2.set("")
+p2 = ttk.Combobox(root, value=sorted(top_lists['producers_list']), textvariable=e_procom2)
+p2.grid(row=12, column=2, sticky=tk.E, padx=3)
+# production_company 3
+l_procom3 = tk.Label(root, text='Select Producer 3:')
+l_procom3.grid(row=13, sticky=tk.W)
+e_procom3 = tk.StringVar(root)
+e_procom3.set("")
+p3 = ttk.Combobox(root, value=sorted(top_lists['producers_list']), textvariable=e_procom3)
+p3.grid(row=13, column=2, sticky=tk.E, padx=3)
+
+
+# 'production_countries' 1
+l_procount1 = tk.Label(root, text='Select Country 1:')
+l_procount1.grid(row=14, sticky=tk.W)
+e_procount1 = tk.StringVar(root)
+e_procount1.set("")
+pr1 = ttk.Combobox(root, value=sorted(top_lists['countries_list']), textvariable=e_procount1)
+pr1.grid(row=14, column=2)
+# 'production_countries' 2
+l_procount2 = tk.Label(root, text='Select Country 2:')
+l_procount2.grid(row=15, sticky=tk.W)
+e_procount2 = tk.StringVar(root)
+e_procount2.set("")
+pr2 = ttk.Combobox(root, value=sorted(top_lists['countries_list']), textvariable=e_procount2)
+pr2.grid(row=15, column=2)
+
+
+# 'spoken_languages 1
+l_spl1 = tk.Label(root, text='Select Language 1:')
+l_spl1.grid(row=16, sticky=tk.W)
+e_spl1 = tk.StringVar(root)
+e_spl1.set("")
+l1 = ttk.Combobox(root, value=sorted(top_lists['lang_list']), textvariable=e_spl1)
+l1.grid(row=16, column=2)
+# 'spoken_languages 2
+l_spl2 = tk.Label(root, text='Select Language 2:')
+l_spl2.grid(row=17, sticky=tk.W)
+e_spl2 = tk.StringVar(root)
+e_spl2.set("")
+l2 = ttk.Combobox(root, value=sorted(top_lists['lang_list']), textvariable=e_spl2)
+l2.grid(row=17, column=2)
+
+
+# predict button
+f_btn = tk.Frame(root)
+b_login = tk.Button(f_btn, text='pred', width=6)
+b_login.grid(row=18, column=5)
+b_cancel = tk.Button(f_btn, text='cancel', width=6, command=root.quit)
+b_cancel.grid(row=18, column=10)
+f_btn.grid(row=18, columnspan=2, pady=10)
+root.mainloop()
